0cb30e3e016c96e582fa3a73d24322a4cb269eb3
Евгений <evgenij.romanko@neoseo.com.ua>
2020-04-11 11:03:33 +0300

    edit package.json

---
+++ build/precache-manifest.7a0de020b834b4fe80168a54cf4ca809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7426a5203ed8b81d470835eea93433b",
+    "url": "/react-hw-01-component/index.html"
+  },
+  {
+    "revision": "6cceff25f33e1a5fb128",
+    "url": "/react-hw-01-component/static/css/main.2222d2d0.chunk.css"
+  },
+  {
+    "revision": "625dbe828102fb99044d",
+    "url": "/react-hw-01-component/static/js/2.41ca4c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-component/static/js/2.41ca4c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cceff25f33e1a5fb128",
+    "url": "/react-hw-01-component/static/js/main.21d8c1c3.chunk.js"
+  },
+  {
+    "revision": "e03a2fa1cdf066fc9019",
+    "url": "/react-hw-01-component/static/js/runtime-main.e1bbf624.js"
+  }
+]);
